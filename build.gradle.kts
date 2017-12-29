9742122d1ec4318ae24039e4d01f88e95b8a7f8e
Petter Remen <petter.remen@gmail.com>
2017-12-29 19:20:48 +0100

    Attach sources when publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,10 +58,19 @@ configure<JUnitPlatformExtension> {
     }
 }
 
+// Create sources Jar from main kotlin sources
+val sourcesJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles sources JAR"
+    classifier = "sources"
+    from(java.sourceSets["main"].allSource)
+}
+
 publishing {
     publications {
         create("default", MavenPublication::class.java) {
             from(components["java"])
+            artifact(sourcesJar)
         }
     }
 }
